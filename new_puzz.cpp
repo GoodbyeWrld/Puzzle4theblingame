3cb982f3d3296b2326ab22ec049918fc177acd36
GoodByeWrld <adrianbrooks99@gmail.com>
2021-11-30 02:00:46 -0500

    DId some cleaning up and also finished the map class and got it to return a pointer with getptrtomap, should be tendered to playscap class

--- new_puzz.cpp
+++ new_puzz.cpp
@@ -2,214 +2,147 @@
 #include<random>
 
 
-
 class want_to_play
-
 {
-
-int num_of_plays = 0;
-
-std::string playorno;
-
-want_to_play()
-
-{
-
-std::cout << "Would you like to play"<<std::endl; //can you c in this from main
-
-std::cin>>playorno;
-
-if(playorno == "yes")
-
-{
-
-num_of_plays++; //may have to return value and add to func header
-}
-
-//in the main ave playorno variable =getplayorno
-
-}
-
- 
-
-void play_again() //can you c in this from main
-
-{
-
-std::cout<<" Wanna play again?"<< std::endl;
-
-num_of_plays++; //may have to return value and add to func header
-
-}//could also create  aget func can call in both construct and play again func
+    private:
+    int num_of_plays = 0;
+    std::string playorno;
+    
+    public:
+    want_to_play()
+    {
+    std::cout << "Would you like to play"<<std::endl; //can you c in this from mai
+    std::cin>>playorno;
+    if(playorno == "yes")
+    {
+        num_of_plays++; //may have to return value and add to func header
+    };
+    }//in the main ave playorno variable =getplayorno
+    
+    void play_again() //can you c in this from main
+    {
+    std::cout<<" Wanna play again?"<< std::endl;
+    num_of_plays++; //may have to return value and add to func header
+    }//could also create  aget func can call in both construct and play again func
 
 };
-
-
- 
-
- 
-
- 
 
 class Map 
-
 {
-std::string difficulty;
-int array_size;
-//char maparr[array_size][array_size];
-char *Ptrto_map;
+    int difficulty;
+    int array_sizex;
+    int array_sizey;
+    //char maparr[array_size][array_size];
+    char *Ptrto_map;
 
-Map()
-{
-std::cout << "what difficulty would you like"<<std::endl;
-std::cin >> difficulty;
-//array_size = //convert diffiluty to size of arra
-Ptrto_map = Create_map(difficulty);
+    Map()
+    {
+        while (difficulty!=1||2||3)
+        {
+            std::cout << "what difficulty would you like"<<"Enter (1) for easy, (2) for medium, (3) for hard"<<std::endl;
+            std::cin >> difficulty;
+            if (difficulty == 1)
+            {
+                array_sizex=12;
+                array_sizey=12;
+            }
+            if (difficulty = 2)
+            {
+                array_sizex=20;
+                array_sizey=20;
+            }
+            if (difficulty = 3)
+            {
+                array_sizex=32;
+                array_sizey=32; 
+            }
+            else
+            {
+                std::cout<<"Incorrect input needs to be 1,2, or 3: "<<std::endl;
+            }
+        }
+    
+    }
+    //array_size = //convert diffiluty to size of array
+     
+    char* Create_map()
+    {
+    //Use char maparr = // Create an array according to difficulty specified fill with char
+    char maparr[array_sizex][array_sizey]; //update
+    //char *ptrtomap = maparr;
+    //return char* array;
+    for (size_t i = 0; i < array_sizex; i++)
+    {
+        for (size_t i2 = 0; i2 < array_sizex; i2++)
+        maparr[i][i2]='.'; 
+    }
+    return *maparr;
+    };
 
-}
-
- 
-
-char* Create_map(std::string)
-{
-
-//Use char maparr = // Create an array according to difficulty specified fill with char
-char maparr; //update
-//char *ptrtomap = maparr;
-//return char* array;
-
+    char* getptrtomap ()
+    {
+        return Create_map();
+    }
 };
 
-char* getptrtomap ()
-{
-    //return *ptrtomap;
-}
-
-};
-
- 
-
-
-
- 
 
 class fObject
-
 {
-std::string name_obj;
-
-char char_obj;
-
-char char_obj;
-
-fObject()
-{
-
-//random determination of rock water or fire
-
-//name_obj = Depstd::endling on this decide std::string name
-
-//setchar_ob(std::string);
-
-//Subobj(char obj)
-
- 
-
+    std::string name_obj;
+    char char_obj;
+    char char_obj;
+    fObject()
+    {
+    //random determination of rock water or fir
+    //name_obj = Depstd::endling on this decide std::string nam
+    //setchar_ob(std::string)
+    //Subobj(char obj
+    }
+    char Subobj(char obj); // returns lower case letter of what ever letter
+    std::string getname_ob();
+    void setchar_ob(std::string); //sets the first letter of the std::string std::string_obj to char_obj
+    char getchar(); //add characteristics to points surrounding whatever positions are assigned to a char based off the char
+    void playObjsound(char);
 };
 
- 
-
-char Subobj(char obj); // returns lower case letter of what ever letter
-
-std::string getname_ob();
-
-void setchar_ob(std::string); //sets the first letter of the std::string std::string_obj to char_obj
-
-char getchar(); //add characteristics to points surrounding whatever positions are assigned to a char based off the char
-
-void playObjsound(char);
-
- 
-
- 
-
-};
-
- 
-
- 
- 
 
 class wObject
-
 {
-std::string name_obj;
-
-char char_obj;
-
-char char_obj;
-
- 
-
-
-wObject()
-{
-
-//random determination of rock water or fire
-
-//name_obj = Depstd::endling on this decide std::string name
-
-//setchar_ob(std::string);
-
-//Subobj(char obj)
-
- 
- }
-
- char Subobj(char obj); // returns lower case letter of what ever letter
- std::string getname_ob();
- void setchar_ob(std::string); //sets the first letter of the std::string std::string_obj to char_obj
- char getchar(); //add characteristics to points surrounding whatever positions are assigned to a char based off the char
- void playObjsound(char);
-
- };
-
- 
-
- 
-
-
- 
-
-class rObject
-
-{
-
-std::string name_obj;
-
-char char_obj;
-
-char char_obj;
-
-
-rObject()
-{
-//random determination of rock water or fire
-
-//name_obj = Depstd::endling on this decide std::string name
-
-//setchar_ob(std::string);
-
-//Subobj(char obj)
-
-
+    std::string name_obj;
+    char char_obj;
+    char char_obj;
+    wObject()
+    {
+        //random determination of rock water or fire
+        //name_obj = Depstd::endling on this decide std::string name
+        //setchar_ob(std::string);
+        //Subobj(char obj)
+    }
+    char Subobj(char obj); // returns lower case letter of what ever letter
+    std::string getname_ob();
+    void setchar_ob(std::string); //sets the first letter of the std::string std::string_obj to char_obj
+    char getchar(); //add characteristics to points surrounding whatever positions are assigned to a char based off the char
+    void playObjsound(char);
 };
 
-char Subobj(char obj); // returns lower case letter of what ever letter
-std::string getname_ob();
-void setchar_ob(std::string); //sets the first letter(Capital) of the std::string std::string_obj to char_obj
-char getchar(); //add characteristics to points surrounding whatever positions are assigned to a char based off the char
-void playObjsound(char);
+ 
+class rObject
+{
+    std::string name_obj;
+    char char_obj;
+    char char_obj;
+    rObject()
+    {
+        //random determination of rock water or fire
+        //name_obj = Depstd::endling on this decide std::string nam
+        //setchar_ob(std::string)
+        //Subobj(char obj)
+    }
+    char Subobj(char obj); // returns lower case letter of what ever lette
+    std::string getname_ob();
+    void setchar_ob(std::string); //sets the first letter(Capital) of the std::string std::string_obj to char_ob
+    char getchar(); //add characteristics to points surrounding whatever positions are assigned to a char based off the cha
+    void playObjsound(char);
 
 };
 
@@ -219,85 +152,50 @@ void playObjsound(char);
 
 class Playscape
 {
-//char updatedmap_andplayscape[][];
-//char updatedplayscape_andobj[][];
-int sizeof_array;//create function for thi
-int num_obj = sizeof_array/8;
-int playscapeX_max; //find thi
-int playscapeY_max; //find this
-
- 
-
-/*playscape(mapobj().getptrtomap)
-
-{
-
-//function to createplayscape from mapobjget
-
-//updatedmap_andplayscape = resultafterchange;
-
-}*/
-
-
-void Assignobjto_playscape()
-{
-int numeachobj = num_obj/3;
-fObject;
-wObject;
-rObject;
-int objcurr;
-
-while(objcurr<numeachobj)
-{
-
-// Randomly distribute fobj.getchar on a pos in updatedmap_andplayscape as long as it has an x in pos and at least one x in row and column, and pos not greater than pos x max-2 and ymax
-
-//use posxy of fobj and and put subobj in pos surrounding
-
-objcurr++;
-
-}
-
-while(objcurr<numeachobj)
-{
-
-// Randomly distribute robj.getchar on a pos in updatedmap_andplayscape as long as it has an x in pos and at least one x in row and column, and pos not greater than pos x max-2 and ymax
-
-//use posxy of robj and and put subobj in pos surrounding
-
-objcurr++;
-
-}
-
- 
-
-while(objcurr<numeachobj)
-{
-
-// Randomly distribute wobj.getchar on a pos in updatedmap_andplayscape as long as it has an x in pos and at least one x in row and column, and pos not greater than pos x max-2 and ymax
-
-//use posxy of wobj and and put subobj in pos surrounding
-
-objcurr++;
-
-}
-
-// make sure all of these functions are updating the map to updatedplayscape_andobj[][];
-
- 
-
-}
-
- 
-
- 
-
+    //char updatedmap_andplayscape[][]
+    //char updatedplayscape_andobj[][]
+    int sizeof_array;//create function for th
+    int num_obj = sizeof_array/8;
+    int playscapeX_max; //find th
+    int playscapeY_max; //find this
+    Playscape(Map::*Ptrto_map())
+    {
+        //Map::
+        //function to createplayscape from mapobjget
+        //updatedmap_andplayscape = resultafterchange;
+    }
+    void Assignobjto_playscape()
+    {
+        int numeachobj = num_obj/3;
+        fObject;
+        wObject;
+        rObject;
+        int objcurr;
+        while(objcurr<numeachobj)
+        {
+            // Randomly distribute fobj.getchar on a pos in updatedmap_andplayscape as long as it has an x in pos and at least one x in row and column, and pos not greater than pos x max-2 and ymax
+            //use posxy of fobj and and put subobj in pos surrounding
+            objcurr++;
+        }
+        while(objcurr<numeachobj)
+        {
+            // Randomly distribute robj.getchar on a pos in updatedmap_andplayscape as long as it has an x in pos and at least one x in row and column, and pos not greater than pos x max-2 and yma
+            //use posxy of robj and and put subobj in pos surroundin
+            objcurr++;
+        }
+        while(objcurr<numeachobj)
+        {
+            // Randomly distribute wobj.getchar on a pos in updatedmap_andplayscape as long as it has an x in pos and at least one x in row and column, and pos not greater than pos x max-2 and yma
+            //use posxy of wobj and and put subobj in pos surrounding
+            objcurr++;
+        }
+        // make sure all of these functions are updating the map to updatedplayscape_andobj[][];
+    }
 };
 
-
-class move
-
-{
+ 
+ class move
+ {
 
  
  
